700815ec849973ab65e0e7677b849cdeab2e291a
ardhnant <abhayraj386683@gmail.com>
2025-05-15 15:06:42 +0530

    r

--- JavaScript (shradha khapra)/first.js
+++ JavaScript (shradha khapra)/first.js
@@ -1,30 +1,127 @@
-full_name = "tony stark";
-age = 19;
-price = 99.99;
-x = null;
-y = undefined;
-fullname="25"
-console.log(full_name);
+// full_name = "tony stark";
+// age = 19;
+// price = 99.99;
+// x = null;
+// y = undefined;
+// fullname="25"
+// console.log(full_name);
 
-const student={
-  fullname: "Rahul Kumar",
-  age: 20,
-  cgpa: 8.2,
-  isPass: true,
-  // key: value, 
+// const student={
+//   fullname: "Rahul Kumar",
+//   age: 20,
+//   cgpa: 8.2,
+//   isPass: true,
+//   // key: value, 
+// }
+
+
+
+// student["age"]=student["age"]+1;
+
+// console.log(student["age"]);
+
+// const profile={
+//   username: "@eyddee",
+//   isFollow: true,
+//   followers: "556k",
+//   following: "4",
+// }
+
+// console.log(typeof profile["isFollow"])
+
+// let age = 16
+// if (age>18){
+//   console.log("you can vote")
+// }
+
+// if(age<18){
+//   console.log("you cannot vote")
+// }
+
+
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark") {
+//   color = "black";
+// }
+
+// if (mode === "light") { 
+//   color = "white";
+// }
+
+// console.log(color);
+
+
+// let age = 18
+// if (age >= 18){
+//   console.log("you are ready to vote")
+// } else {
+//   console.log("you are not eligble to vote")
+// }
+
+// let num = 10
+// if (num%2 === 0) {
+//   console.log(num, 'is even')
+// } else {
+//   console.log("i am illitrate")
+// }
+
+// let age = 30
+// if (age <= 24){
+//    console.log("You are a fucking newbie")
+// } else if (age >= 60) {
+//   console.log("You are an old geezer")
+// } else {
+//   console.log ("You are a virgin")
+// }
+
+// let num = prompt("Enter a number");
+// if (num%5 === 0){
+//   console.log("you are right man")
+// } else {
+//   console.log("duck that shi")
+// }
+
+
+// let marks = prompt("Enter your marks");
+
+// if (marks <= 49){
+//   marks = "You got a F grade"
+// } else if (marks <= 59){
+//   marks = "You got a D grade"
+// } else if (marks <= 69){
+//   marks = "You got a C grade"
+// } else if (marks <= 89){
+//   marks = "You got a B grade"
+// } else if (marks <= 100){
+//   marks = "You got a A grade"
+// } else {
+//   marks = "Fuck OFF"
+// } 
+
+// let marks = prompt("Enter your marks");
+
+// if (marks >= 90 && marks <=100 ){
+//   grade = "You got a A grade"
+// } else if (marks >= 70){
+//   grade = "You got a B grade"
+// } else if (marks >= 60){
+//   grade = "You got a C grade"
+// } else if (marks >= 50){
+//   grade = "You got a D grade"
+// } else if (marks >= 0){
+//   grade = "You got a F grade"
+// } else {
+//   grade = "Fuck OFF"
+// } 
+
+// console.log(grade);
+
+
+let sum = 0
+for(let i=1; i <= 10; i++){
+  sum = sum * i
 }
 
-
-
-student["age"]=student["age"]+1;
-
-console.log(student["age"]);
-
-const profile={
-  username: "@eyddee",
-  isFollow: true,
-  followers: "556k",
-  following: "4",
-}
-
-console.log(typeof profile["isFollow"])
+console.log("sum =", sum)
