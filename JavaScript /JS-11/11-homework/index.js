e01710bf3a6f5c96ff795c82e68f2eb696e459ca
ardhnant <abhayraj386683@gmail.com>
2025-05-31 10:59:26 +0530

    yoo

---
+++ JavaScript /JS-11/11-homework/index.js
@@ -0,0 +1,52 @@
+// const myArray = []
+// let minValue =  myArray[0];
+// let maxValue =  myArray[0];
+
+// for ( let i = 0; i < myArray.length; i++){
+//   if ( myArray[i] < minValue ){
+//     minValue = myArray[i];
+    
+//   }
+// }
+
+// console.log(`Minimum Value is ${minValue}`);
+
+// for ( let i = 0; i < myArray.length; i++){
+//   if ( myArray[i] > maxValue ){
+//     maxValue = myArray[i];
+//   }
+// }
+
+// console.log(`Maximum value is ${maxValue}`);
+
+// function minMax(nums) {
+//   if (nums.length === 0) {
+//     return { min: null, max: null };
+//   }
+
+//   let min = nums[0];
+//   let max = nums[0];
+
+//   for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] < min) {
+//       min = nums[i];
+//     }
+//     if (nums[i] > max) {
+//       max = nums[i];
+//     }
+//   }
+
+//   return { min: min, max: max };
+// }
+// console.log(minMax([1, -3, 5]));
+// console.log(minMax([-2, 3, -5, 7, 10]));
+// console.log(minMax([3]));
+// console.log(minMax([]));
+
+myArray = ['apple', 'grape', 'apple', 'apple'];
+
+let wordCount = 0
+
+for (let i = 1; i < myArray.length; i++){
+  if (myArray[i] === )
+}
