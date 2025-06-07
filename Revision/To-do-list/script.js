a31c8cc1ab3c139a9aea79cfff7c35ab415bb21e
ardhnant <eydde@proton.me>
2025-06-07 13:20:49 +0530

    Auto Button

--- Revision/To-do-list/script.js
+++ Revision/To-do-list/script.js
@@ -3,14 +3,27 @@ const inputDate = document.querySelector('#todo-date');
 const addBtn = document.querySelector('#add-btn');
 const chores = document.querySelector('#chores');
 
-let html = `<div id="chore"><p id="chore-content">${inputText.value}</p><p id="chore-content">${inputDate.value}</p> <button id="delete-btn">Delete</button>`
-
-
 addBtn.addEventListener('click', () => {
   if(inputText.value === '' || inputDate.value === '' ){
     console.log('please input some thing.');
   } else {
+
+    let html = `<div class="chore"><p id="chore-content">${inputText.value}</p><p id="chore-content">${inputDate.value}</p> <button class="delete-btn">Delete</button>`;
+
     document.querySelector('#chores').innerHTML += html;
     inputText.value = '';
+    inputDate.value = '';
   }
-})
+});
+
+chores.addEventListener('click', (event) => {
+  if(event.target.classList.contains('delete-btn')){
+    event.target.parentElement.remove();
+  }
+});
+
+// document.querySelectorAll('#delete-btn').forEach((deleteBtn, i) => {
+//   deleteBtn.addEventListener('click', () => {
+//     chores.style.display = 'none';
+//   });
+// });
